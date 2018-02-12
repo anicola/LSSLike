d8993740c322441e419c1e3cec61e9072bfb2f2c
Anže Slosar <anze@bnl.gov>
2018-02-12 16:30:13 -0500

    kinda working version

--- desclss/lss_theory.py
+++ desclss/lss_theory.py
@@ -4,20 +4,21 @@ import sacc
 from scipy.interpolate import interp1d
 
 class LSSTheory(object):
-    def __init__(self,data_file, log_normal=True) :
-        self.s=sacc.SACC.loadFromHDF(data_file)
+    def __init__(self,data_sacc, log_normal=True,zmax=None) :
+        self.s=data_sacc
         self.log_normal=log_normal
         self.ln_dz=0.05 ## fixed for now for lognorm predictions
         self.ln_kmin=1e-4
         self.ln_kmax=50.
         self.ln_Nk=65536
         
-        if self.s.binning==None :
+        if self.s.binning is None:
             raise ValueError("Binning needed!")
-        # get zmax
-        zmax=-1
-        for tr in self.s.tracers :
-            zmax=max(zmax,tr.z.max())
+
+        if zmax is None:
+            zmax=-1
+            for tr in self.s.tracers :
+                zmax=max(zmax,tr.z.max())
         self.zmax=zmax
         
     def bias_interpolators(self,dic_par):
@@ -28,6 +29,7 @@ class LSSTheory(object):
                     z_b_arr=dic_par[tr.exp_sample+'_z_b']
                     b_b_arr=dic_par[tr.exp_sample+'_b_b']
                 except:
+                    print "Need",tr.exp_sample,'+_z_b,_b_b'
                     raise ValueError("bias needed for each tracer")
 
                 # bias function of the input parameters
@@ -44,16 +46,16 @@ class LSSTheory(object):
         else:
             bfuncs=self.bias_interpolators(dic_par)
             bias_arrs=[bf(tr.z) for (bf,tr) in zip(bfuncs,tracers)]
-            
-        ccl_tracers=[ccl.ClTracerNumberCounts(cosmo,False,False,tr.z,tr.Nz,tr.z,b_arr)
+
+        ccl_tracers=[ccl.ClTracer(cosmo,'nc',has_rsd=False,has_magnification=False,
+                                  n=(tr.z,tr.Nz), bias=(tr.z,b_arr))
                 for (tr,b_arr) in zip(tracers,bias_arrs)]
         return ccl_tracers
     
-    def get_cosmo(self, dic_par, tracers) :
+    def get_params(self, dic_par) :
         omega_c = dic_par['omega_c']
         omega_b = dic_par['omega_b']
         omega_k = dic_par['omega_k']
-        omega_nu = dic_par['omega_nu']
         if 'w' in dic_par :
             w = dic_par['w']
         else :
@@ -62,7 +64,7 @@ class LSSTheory(object):
             wa = dic_par['wa']
         else :
             wa = 0.
-        h0 = dic_par['h0']
+        h0 = dic_par['h']
         has_sigma8 = ('sigma_8' in dic_par)
         has_A_s = ('A_s' in dic_par)
         n_s = dic_par['n_s']
@@ -71,17 +73,19 @@ class LSSTheory(object):
         elif has_sigma8:
             sigma8=dic_par['sigma_8']
             params=ccl.Parameters(Omega_c=omega_c,Omega_b=omega_b,Omega_k=omega_k,
-                                  Omega_n=omega_nu,w0=w,wa=wa,sigma8=sigma8,n_s=n_s,h=h0)
+                                  w0=w,wa=wa,sigma8=sigma8,n_s=n_s,h=h0)
         elif has_A_s:
             A_s = dic_par['A_s']
             params = ccl.Parameters(Omega_c=omega_c,Omega_b=omega_b,Omega_k=omega_k,
-                                    Omega_n=omega_nu,w0=w,wa=wa,A_s=A_s,n_s=n_s,h=h0)
+                                    w0=w,wa=wa,A_s=A_s,n_s=n_s,h=h0)
         else:
             raise ValueError("Need either sigma 8 or A_s in pyccl.")
 
+        return params
 
+    def get_cosmo(self, params,dic_par):
         if not self.log_normal:
-            cosmo=ccl.Cosmology(params,
+            mcosmo=ccl.Cosmology(params,
                             transfer_function=dic_par['transfer_function'],
                             matter_power_spectrum=dic_par['matter_power_spectrum'])
         else:
@@ -99,34 +103,40 @@ class LSSTheory(object):
             ## actually generate a cosmology for each pair of tracers
             ## master cosmology to pull out matter power spectra
             mcosmo=ccl.Cosmology(params,
-                            transfer_function=dic_par['transfer_function'],
-                            matter_power_spectrum=dic_par['matter_power_spectrum'])
+                                transfer_function=dic_par['transfer_function'],
+                                 matter_power_spectrum='linear')
             bfuncs=self.bias_interpolators(dic_par)
             assert (len(self.s.get_exp_sample_set())==1)
             ks=np.logspace(np.log10(self.ln_kmin),np.log10(self.ln_kmax),self.ln_Nk)
-            zs=np.arange(0.,self.zmax+self.ln_dz,self.ln_dz)
+            zs=np.array(np.arange(0.,self.zmax,self.ln_dz))[::-1]
             pk_lins=[ccl.linear_matter_power(mcosmo,ks,a) for a in 1./(1+zs)]
-            rsm2=dic_par['rsm2']/(params['h']*params['h'])
-            lntpk=[]
-            for z,pk_lin in zip(zs,pk_lins):
+            rsm2=(dic_par['rsm']/params['h'])**2
+            pk2d=np.zeros([len(zs),len(ks)])
+            for zi,(z,pk_lin) in enumerate(zip(zs,pk_lins)):
                 b=bfuncs[0](z) # here we assume that all tracers have same bias
                 pkt=pk_lin*b*b*np.exp(-ks*ks*rsm2)
-                r_lin,xi_lin=ccl.pk2xi(ks,pk_lin)
+                r_lin,xi_lin=ccl.utils.pk2xi(ks,pkt)
                 xi_t=np.exp(xi_lin)-1
-                _,pkt=ccl.xi2pk(r_lin,xi_t)
+                _,pkt=ccl.utils.xi2pk(r_lin,xi_t)
                 pkt=fix_pk(ks,pkt)
-                lntpk.append(pkt)
-                    
-            ccl.update_matter_power(mcosmo,ks,1./(1+zs),lntpk,is_linear=True)
-            ccl.update_matter_power(mcosmo,ks,1./(1+zs),lntpk,is_linear=False)
+                pk2d[zi,:]=pkt
+                
+            pk2d=pk2d.flatten()
+            aas=1./(1+zs)
+            ccl.update_matter_power(mcosmo,ks,aas,pk2d,is_linear=True)
+            ccl.update_matter_power(mcosmo,ks,aas,pk2d,is_linear=False)
         return mcosmo
 
-    def get_prediction(self,dic_par) :
+    def get_prediction(self,dic_par,diagonal_only=True) :
         theory_out=np.zeros((self.s.size(),))
+        params=self.get_params(dic_par)
+        cosmo=self.get_cosmo(params,dic_par)
         tr=self.get_tracers(cosmo,dic_par)
-        cosmo=self.get_cosmo(dic_par,tr)
+
 
         for cc,(i1,i2,ells,ndx) in enumerate(self.s.sortTracers()):
+            if diagonal_only and (i1!=i2):
+                continue
             cls=ccl.angular_cl(cosmo,tr[i1],tr[i2],ells)
             theory_out[ndx]=cls
             
--- test/print_chi2.py
+++ test/print_chi2.py
@@ -3,13 +3,14 @@ import sacc
 from  desclss import *
 from scipy.stats import chi2
 
-true_pred=sacc.SACC.loadFromHDF("sim_sample/sims/sim_mean.sacc")
+true_pred=sacc.SACC.loadFromHDF("inputs/fastcats/GaussPZ_0.02+FullSky+run0+ztrue_mean.sacc")
 true_vec=true_pred.mean.vector
 Ndof=true_pred.size()
-precision_fn="sim_sample/sims/sim_mean.sacc"
+precision_fn="inputs/fastcats/GaussPZ_0.02+FullSky+run0+ztrue_mean.sacc"
 
-for u in range(10):
-    t=LSSLikelihood(sacc.SACC.loadFromHDF("sim_sample/sims/sim_%03d.sacc"%u,
-                                          precision_filename=precision_fn))
+for u in range(1,10):
+    fn="inputs/fastcats/GaussPZ_0.02+FullSky+run%i+ztrue/twopoints%i_ns2048.sacc"%(u,u)
+    print "fn=",fn
+    t=LSSLikelihood(sacc.SACC.loadFromHDF(fn, precision_filename=precision_fn))
     ch2=t.chi2(true_vec)
     print u,"chi2=",ch2,"/",Ndof,"p=",chi2.cdf(ch2,Ndof)
