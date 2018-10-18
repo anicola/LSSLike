3278044dc688bb34fd573687063fe583f5206a3b
Anze Slosar <slosar@gmail.com>
2018-10-18 12:10:48 -0400

    consistency calc for 4 bins

---
+++ hsc/hsc_consistency.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import sacc
+import matplotlib.pyplot as plt
+import scipy.linalg as la
+from  scipy.stats import chi2 as chi2d
+from copy import deepcopy
+
+def main():
+    if len(sys.argv)<3:
+        print ("Specify at least two files on input")
+        sys.exit(1)
+    fnames=sys.argv[1:]
+    saccsin=[[print ("Loading %s..."%fn),
+           sacc.SACC.loadFromHDF(fn)].pop() for fn in fnames]
+
+    Ntomo=len(saccsin[0].tracers)
+    Lmax=200000
+    
+    plt.figure()
+    if Ntomo==4:
+        Nx=Ny=2
+    elif Ntomo==1:
+        Nx=Ny=1
+
+    for itomo in range(-1,Ntomo):
+        if itomo>=0:
+            print ("Tomographic bin:",itomo)
+        else:
+            print ("All bins together")
+        saccs=[deepcopy(s) for s in saccsin]
+        if itomo<0:
+            lmin=[0]*Ntomo
+            lmax=[Lmax]*Ntomo
+        else:
+            lmin=[100000]*Ntomo
+            lmax=[Lmax]*Ntomo
+            lmin[itomo]=0
+            
+        for s in saccs:
+            s.cullLminLmax(lmin,lmax)
+            
+        mvecs=[s.mean.vector for s in saccs]
+        pmats=[s.precision.getPrecisionMatrix() for s in saccs]
+        cmats=[s.precision.getCovarianceMatrix() for s in saccs]
+        sumws=np.array([np.dot(p,v) for p,v in zip(pmats,mvecs)]).sum(axis=0)
+        sumicov=np.array(pmats).sum(axis=0)
+        sumcov=la.inv(sumicov)
+        mean=np.dot(sumcov,sumws)
+
+        ## Sk tests.
+        chi2=[]
+        dof=len(mean)
+
+        for m,c,s in zip(mvecs,cmats,saccs):
+            diff=(m-mean)
+            C=c-sumcov
+            chi2=np.dot(diff,np.dot(la.inv(C),diff))
+            print 
+            print ("{:20s} {:7.2f} {:7.2f} {:7.4f} ".format(s.tracers[0].exp_sample.replace("'","").replace("b'",""),chi2,dof,1-chi2d(df=dof).cdf(chi2)))
+
+
+        if (itomo>=0):
+            plt.subplot(Nx,Ny,itomo+1)
+            plotDataTheory(saccs,mean)
+
+    plt.show()
+    
+def plotDataTheory (saccs,mean):
+    clrcy='rgbycmk'
+    for i,s in enumerate(saccs):
+        s.plot_vector(out_name=None,clr=clrcy[i],lofsf=1.01**i,
+                      label=s.tracers[0].exp_sample)
+    els=saccs[0].binning.binar['ls']
+    plt.plot(els,mean,'k-',lw=2)
+
+if __name__=="__main__":
+    main()
+    
+
+    
+
