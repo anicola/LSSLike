3f2f51e8952cca517296fef20fa65e9fc0302472
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2018-10-26 06:08:33 -0700

    Added h0 fitting, modified some default fitting values, and fixed some bugs

--- hsc/hsc_driver.py
+++ hsc/hsc_driver.py
@@ -15,12 +15,13 @@ from ParamVec import ParamVec
 class HSCAnalyze:
 
     def __init__(self, fnames, lmin='auto', lmax='auto',
-                 fitOc=False, Oc=0.25,
-                 fits8=False, s8=0.8,
-                 fitBias=True,
+                 fitOc=True, Oc=0.25,
+                 fits8=True, s8=0.8,
+                 fith0=True, h0 = 0.6774,
+                 fitBias=False,
                  zbias=[0.0,0.5,1.0,2.0,4.0],
                  bias=[0.7,1.5,1.8,2.0,2.5],
-                 fitNoise=True, noise=0.75, ## in units of 1e-8
+                 fitNoise=False, noise=0.75, ## in units of 1e-8
                  fitPZShifts=False,
                  pzshifts=[0,0,0,0],
                  log=logging.DEBUG):
@@ -37,7 +38,7 @@ class HSCAnalyze:
             self.log.addHandler(ch)
             print (self.log)
             
-        self.saccs=[sacc.SACC.loadFromHDF(fn) for fn in sys.argv[1:]]
+        self.saccs=[sacc.SACC.loadFromHDF(fn) for fn in fnames]
         self.log.info ("Loaded %i sacc files."%len(self.saccs))
 
         self.Ntomo=len(self.saccs[0].tracers) ## number of tomo bins
@@ -45,7 +46,7 @@ class HSCAnalyze:
 
         self.fixnames()
         self.cutLranges(lmin,lmax)
-        self.setParametes(fitOc,Oc,fits8,s8,fitBias,zbias,bias,fitNoise,noise,fitPZShifts,pzshifts)
+        self.setParametes(fitOc,Oc,fits8,s8,fith0,h0,fitBias,zbias,bias,fitNoise,noise,fitPZShifts,pzshifts)
             
         self.lts=[LSSTheory(s) for s in self.saccs]
         self.lks=[LSSLikelihood(s) for s in self.saccs]
@@ -83,12 +84,14 @@ class HSCAnalyze:
         for s in self.saccs:
             s.cullLminLmax(self.lmin,self.lmax)
 
-    def setParametes(self,fitOc,Oc,fits8,s8,fitBias,zbias,bias,fitNoise,noise,fitPZShifts,pzshifts):
+    def setParametes(self,fitOc,Oc,fits8,s8,fith0,h0,fitBias,zbias,bias,fitNoise,noise,fitPZShifts,pzshifts):
         #### set up parameters
         self.fitOc=fitOc
         self.Oc=Oc
         self.fits8=fits8
         self.s8=s8
+        self.h0=h0
+        self.fith0=fith0
         self.fitBias=fitBias
         self.zbias=zbias
         self.bias=bias
@@ -102,6 +105,8 @@ class HSCAnalyze:
             self.P.addParam('Oc',Oc,'$\\Omega_c$',min=0.2,max=0.4)
         if self.fits8:
             self.P.addParam('s8',s8, '$\sigma_8$',min=0.1,max=2.0)
+        if self.h0:
+            self.P.addParam('h0',h0, '$h_0$',min=0.65,max=0.75)
         if self.fitBias:
             for z,b in zip(self.zbias,self.bias):
                 self.P.addParam('b_%2.1f'%z,b,min=0.5,max=5) 
@@ -121,13 +126,14 @@ class HSCAnalyze:
     def predictTheory(self,p):
         P=self.P.clone()
         P.setValues(p)
-        oc=P.value(Oc) if self.fitOc else self.Oc
-        s8=P.value(s8) if self.fits8 else self.s8
+        oc=P.value('Oc') if self.fitOc else self.Oc
+        s8=P.value('s8') if self.fits8 else self.s8
+        h0=P.value('h0') if self.fith0 else self.h0
         dic={'Omega_c':oc,
          'Omega_b':0.0486,
          'Omega_k':0.0,
          'Omega_nu':0.001436176,
-         'h0':0.6774,
+         'h0':h0,
          'n_s':0.96,
          'sigma_8':s8,
          'transfer_function':'eisenstein_hu',
@@ -137,7 +143,7 @@ class HSCAnalyze:
             dic.update({'hscgals_z_b':self.zbias,
                         'hscgals_b_b':[P.value('b_%2.1f'%z) for z in self.zbias]})
         else:
-            dic.update({'hscgals_z_b':zbias,
+            dic.update({'hscgals_z_b':self.zbias,
                         'hscgals_b_b':self.bias})
             
         if self.fitNoise:
